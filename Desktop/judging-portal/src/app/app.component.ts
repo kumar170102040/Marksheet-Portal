f034185db0a354094ea097c13967ac4e6688e40a
sjain24 <jain170107062@gmail.com>
2018-12-19 15:04:09 +0530

    added max marks row and an attempt to add columns via button

--- Desktop/judging-portal/src/app/app.component.ts
+++ Desktop/judging-portal/src/app/app.component.ts
@@ -1,25 +1,44 @@
 import { Component } from '@angular/core';
-
+declare var jquery:any;
+declare var $ :any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   TableData: any = [];
+  public maxMarks: any = [];
   ShowEditTable: boolean = false;
   EditRowID: any ='';
   constructor(){
-    this.TableData =[
-    {id: 1 ,registrationNo: '11029032' , criterion1: '6' , criterion2: '9', criterion3: '9'},
-    {id: 2 ,registrationNo: '11029033' , criterion1: '7' , criterion2: '8', criterion3: '8'},
-    {id: 3 ,registrationNo: '11029034' , criterion1: '7' , criterion2: '8', criterion3: '7'},
-    {id: 4 ,registrationNo: '11029035' , criterion1: '8' , criterion2: '5', criterion3: '10'},
-  ];
+    this.TableData =
+    [
+    {id: 1 ,registrationNo: '11029032' , criterion1: '6' , criterion2: '9', criterion3: '9', criterion4: '9' ,criterion5: '9',criterion6: '9'},
+    {id: 2 ,registrationNo: '11029033' , criterion1: '7' , criterion2: '8', criterion3: '8', criterion4: '9' ,criterion5: '9',criterion6: '9'},
+    {id: 3 ,registrationNo: '11029034' , criterion1: '7' , criterion2: '8', criterion3: '7', criterion4: '9' ,criterion5: '9',criterion6: '9'},
+    {id: 4 ,registrationNo: '11029035' , criterion1: '8' , criterion2: '5', criterion3: '10', criterion4: '9' ,criterion5: '9',criterion6: '9'},
+    ];
 
-
+    this.maxMarks ={
+        mm1:10, 
+        mm2:10, 
+        mm3:10, 
+        mm4:10, 
+        mm5:10,
+    }
   }
   Edit(val){
     this.EditRowID = val;
   }
+  /*******this part is still under work**********/
+  /*method to add columns********/
+  /*add_col(){
+    $("thead").append("<th>Criterion-5</th>");
+    $("tr").append(`
+          <td *ngIf = "user.id === EditRowID" ><input type="text" [(ngModel)]="user.criterion7"></td>
+          <td *ngIf = "user.id !== EditRowID" (click)="Edit(user.id)">--</td>         
+        `);
+   }*/
+   
 }

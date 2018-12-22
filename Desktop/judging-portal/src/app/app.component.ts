2609c92c389c2542e0cdbac8b3704e6e2bed2654
sjain24 <jain170107062@gmail.com>
2018-12-22 17:32:30 +0530

    marksheet using reactive forms

--- Desktop/judging-portal/src/app/app.component.ts
+++ Desktop/judging-portal/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder , FormArray , FormControl , FormGroup , FormGroupName } from '@angular/forms';
+
 declare var jquery:any;
 declare var $ :any;
 @Component({
@@ -7,18 +9,18 @@ declare var $ :any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
+  tableForm: FormGroup;
   TableData: any = [];
   public maxMarks: any = [];
   ShowEditTable: boolean = false;
   EditRowID: any ='';
-  constructor(){
-    this.TableData =
-    [
-    {id: 1 ,registrationNo: '11029032' , criterion1: '6' , criterion2: '9', criterion3: '9', criterion4: '9' ,criterion5: '9',criterion6: '9'},
-    {id: 2 ,registrationNo: '11029033' , criterion1: '7' , criterion2: '8', criterion3: '8', criterion4: '9' ,criterion5: '9',criterion6: '9'},
-    {id: 3 ,registrationNo: '11029034' , criterion1: '7' , criterion2: '8', criterion3: '7', criterion4: '9' ,criterion5: '9',criterion6: '9'},
-    {id: 4 ,registrationNo: '11029035' , criterion1: '8' , criterion2: '5', criterion3: '10', criterion4: '9' ,criterion5: '9',criterion6: '9'},
-    ];
+  constructor(private fb: FormBuilder){
+    this.tableForm = this.fb.group({
+      registrationNo: new FormControl({value:'', disabled : true}), 
+      criterion1: [''],
+      OtherCriterion: this.fb.array([])
+    });
+
 
     this.maxMarks ={
         mm1:10, 
@@ -28,17 +30,26 @@ export class AppComponent{
         mm5:10,
     }
   }
+
+  
+  get OtherCriterion(){
+    return this.tableForm.get('OtherCriterion') as FormArray;
+  }
+
+  
+  addCriterion() {
+    this.OtherCriterion.push(this.fb.control(''));
+  }
+
   Edit(val){
     this.EditRowID = val;
   }
-  /*******this part is still under work**********/
-  /*method to add columns********/
-  /*add_col(){
-    $("thead").append("<th>Criterion-5</th>");
-    $("tr").append(`
-          <td *ngIf = "user.id === EditRowID" ><input type="text" [(ngModel)]="user.criterion7"></td>
-          <td *ngIf = "user.id !== EditRowID" (click)="Edit(user.id)">--</td>         
-        `);
-   }*/
+
+  loadAPIData() {
+    this.tableForm.patchValue({
+      registrationNo: 'Bruce',
+      criterion1:'0'
+    });
+  }
    
 }
--- Desktop/judging-portal/src/app/app.module.ts
+++ Desktop/judging-portal/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

a2437a7d24019b352f572526bbfe17158c3a6f9a
sjain24 <jain170107062@gmail.com>
2018-12-29 18:37:40 +0530

    Added tabular view and hide method
    add crtiteria button not working due to logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
+import { FormGroup, FormArray, FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,17 @@ import { FormGroup, FormArray, FormBuilder } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
     public myForm: FormGroup;
-    participants=[
+    public mm = 10;
+    public totalCriteria = 1;
+    EditRowId: any ='';
+    arr2 = [{
+      'criteria':"0",
+    },
+    {
+      'criteria':"0",
+    },
+    ];
+    participant_data=[
       {
         'registrationNo':"001"
       },
@@ -19,28 +29,44 @@ export class AppComponent implements OnInit {
       },
       {
         'registrationNo':"004"
-      }       
+      },   
+      {
+        'registrationNo':"005"
+      }    
     ]
+    
 
     constructor(private fb: FormBuilder) { }
 
     ngOnInit() {
         var arr = [];
-        for(var i = 0; i < this.participants.length; i++){
-          arr.push(this.buildParticipant(this.participants[i]));
+        for(var i = 0; i < this.participant_data.length; i++){
+          arr.push(this.buildParticipant(this.participant_data[i] , i));
         }
         this.myForm = this.fb.group({
             participants: this.fb.array(arr)
         });
     }
 
-    buildParticipant(participants): FormGroup{
+    buildParticipant(participants , index): FormGroup{
+      
+
       return this.fb.group({
+        id: [index +1],
         registrationNo: [participants.registrationNo],
         criteria1:[''],
         Othercriteria: this.fb.array([])
       })
     }
+
+    buildCriterion(): FormGroup{
+      return this.fb.group({
+        criteria:['']
+      })
+    }
+
+    
+
 
     get Participants(){
       return this.myForm.get('participants') as FormArray;
@@ -51,39 +77,20 @@ export class AppComponent implements OnInit {
   
     
     addCriterion() {
-      this.Othercriteria.push(this.fb.control(''));
+      //  this.Othercriteria.push(this.fb.control(''));
+      this.totalCriteria = this.totalCriteria  + 1;
+      console.log(this.totalCriteria);
+      this.arr2.push({
+        'criteria':"0",
+      });
+      console.log(this.arr2);
     }
 
-    initParticipants() {
-        return this.fb.group({
-            RegistrationNo: [''],
-            criteria: this.fb.array([
-              this.initCriteria()
-            ])
-        });
-    }
+  Edit(val){
+     this.EditRowId = val;
+     //console.log("function called");
+  }
 
-    initCriteria() {
-      return this.fb.group({
-        number: ['']
-      })
-    }
-    
-    addParticipant() {
-      const control = <FormArray>this.myForm.controls['participants'];
-      control.push(this.initParticipants());
-    }
-
-  
-    // addNumber(address): void {
-    //   const control = <FormArray>address.controls.criteria;
-    //   control.push(this.initCriteria());
-    // }
-
-    // addNumbertoAll(): void {
-    //   for(var i=0; i < this.myForm.controls['participants'] ; i++)
-    // }
-  
 
   save(formData) {
     console.log(formData.value)

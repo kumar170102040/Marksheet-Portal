bd97c7243989b2dddf72468d555f7a369794ac93
sjain24 <jain170107062@gmail.com>
2019-01-07 20:40:15 +0530

    Using Http and observables to fetch data
    made a separate service for fetching data . Added http and observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy} from '@angular/core';
 import { FormGroup, FormArray, FormBuilder, FormControl } from '@angular/forms';
+import { DataService } from './data.service';
+import { constructDependencies } from '@angular/core/src/di/reflective_provider';
+
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit{
     public myForm: FormGroup;
     public totalCriteria = 1;
+    public errorMsg;
     EditRowId: any ='';
-   
+    public i:number = 0;
 
-    criteria_data = [{
-      'criteriaName':"criteria1",
-      'mm':'100'
-    },
-    {
-      'criteriaName':"criteria2",
-      'mm':'100'
-    },
-    {
-      'criteriaName':"criteria3",
-      'mm':'100'
-    }
-  
-    ];
-
-    participant_data=[
-      {
-        'registrationNo':"001"
-      },
-      {
-        'registrationNo':"002"
-      },
-      {
-        'registrationNo':"003"
-      },
-      {
-        'registrationNo':"004"
-      },   
-   
-    ]
+    public criteria_data = [];
+    public participant_data=[];
     
+    constructor(private fb: FormBuilder , private _dataservice : DataService) {
+      // console.log(this.i + "c");
+      // this.i = this.i +1;
+      // console.log(this.i + "c");
 
-    constructor(private fb: FormBuilder) { }
+    }
 
     ngOnInit() {
+        this._dataservice.getParticipant()
+        .subscribe(data => this.participant_data = data);
+        this._dataservice.getCriteria()
+        .subscribe(data => this.criteria_data = data,
+                   error => this.errorMsg = error);
+        // console.log(this.participant_data);
+        // console.log(this.criteria_data);
+
+        // console.log(this.i + "i");
+        // this.i = this.i +1;
+        // console.log(this.i + "i");
         var arr = [];
         for(var i = 0; i < this.participant_data.length; i++){
           arr.push(this.buildParticipant(this.participant_data[i] , i));
@@ -54,15 +46,28 @@ export class AppComponent implements OnInit {
         this.myForm = this.fb.group({
             participants: this.fb.array(arr)
         });
-    }
-
+        
+      }
+    loadApidata(){
+        // this.i = this.i +1;
+        // console.log(this.i + "i");
+        var arr = [];
+        for(var i = 0; i < this.participant_data.length; i++){
+          arr.push(this.buildParticipant(this.participant_data[i] , i));
+        }
+        this.myForm = this.fb.group({
+            participants: this.fb.array(arr)
+        });
+      }
+   
     
     buildParticipant(participants , index): FormGroup{
       var arr2 = [];
       for(var j = 0; j < this.criteria_data.length; j++){
         arr2.push(this.buildCriteria(this.criteria_data[j] , j));
       }
-
+      // console.log(this.criteria_data );
+      // console.log(this.participant_data );
       return this.fb.group({
         id: [index +1],
         registrationNo: [participants.registrationNo],
@@ -80,7 +85,10 @@ export class AppComponent implements OnInit {
 
     get Participants(){
       return this.myForm.get('participants') as FormArray;
+      // console.log(this.participant_data);
+      // console.log(this.criteria_data);
     }
+    
     // get Othercriteria(){
     //   return this.Participants.get('Othercriteria') as FormArray;
     // }
@@ -99,14 +107,16 @@ export class AppComponent implements OnInit {
   Edit(val){
      this.EditRowId = val;
      //console.log("function called");
+     
   }
 
 
   save(formData) {
-    console.log(formData.value)
+    console.log(formData.value);
+    // console.log(this.criteria_data );
+    // console.log(this.participant_data );
   }
 
-
- 
-
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [DataService]
 })
-export class AppModule { }
+export class AppModule {}

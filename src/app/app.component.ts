e03ef5cf756a3f91e01ab5f90f3c21926dce24e8
sjain24 <jain170107062@gmail.com>
2019-01-12 00:18:06 +0530

    Added some css effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit ,OnDestroy} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { FormGroup, FormArray, FormBuilder, FormControl } from '@angular/forms';
 import { DataService } from './data.service';
-import { constructDependencies } from '@angular/core/src/di/reflective_provider';
-
-
-
+import { RegistrationService } from './registration.service';
 
 @Component({
   selector: 'app-root',
@@ -14,32 +11,20 @@ export class AppComponent implements OnInit{
     public myForm: FormGroup;
     public totalCriteria = 1;
     public errorMsg;
-    public DataSaved;
     EditRowId: any ='';
     public i:number = 0;
 
     public criteria_data = [];
     public participant_data=[];
     
-    constructor(private fb: FormBuilder , private _dataservice : DataService) {
-      // console.log(this.i + "c");
-      // this.i = this.i +1;
-      // console.log(this.i + "c");
-
-    }
-
+    constructor(private fb: FormBuilder , private _dataservice : DataService , private _registrationService: RegistrationService) {}
     ngOnInit() {
         this._dataservice.getParticipant()
         .subscribe(data => this.participant_data = data);
         this._dataservice.getCriteria()
         .subscribe(data => this.criteria_data = data,
                    error => this.errorMsg = error);
-        // console.log(this.participant_data);
-        // console.log(this.criteria_data);
 
-        // console.log(this.i + "i");
-        // this.i = this.i +1;
-        // console.log(this.i + "i");
         var arr = [];
         for(var i = 0; i < this.participant_data.length; i++){
           arr.push(this.buildParticipant(this.participant_data[i] , i));
@@ -50,8 +35,6 @@ export class AppComponent implements OnInit{
         
       }
     loadApidata(){
-        // this.i = this.i +1;
-        // console.log(this.i + "i");
         var arr = [];
         for(var i = 0; i < this.participant_data.length; i++){
           arr.push(this.buildParticipant(this.participant_data[i] , i));
@@ -67,8 +50,6 @@ export class AppComponent implements OnInit{
       for(var j = 0; j < this.criteria_data.length; j++){
         arr2.push(this.buildCriteria(this.criteria_data[j] , j));
       }
-      // console.log(this.criteria_data );
-      // console.log(this.participant_data );
       return this.fb.group({
         id: [index +1],
         registrationNo: [participants.registrationNo],
@@ -87,47 +68,37 @@ export class AppComponent implements OnInit{
 
     get Participants(){
       return this.myForm.get('participants') as FormArray;
-      // console.log(this.participant_data);
-      // console.log(this.criteria_data);
     }
-    
-    // get Othercriteria(){
-    //   return this.Participants.get('Othercriteria') as FormArray;
-    // }
-  
-    
-    // addCriterion() {
-    //   //this.Othercriteria.push(this.fb.control(''));
-    //   this.totalCriteria = this.totalCriteria  + 1;
-    //   console.log(this.totalCriteria);
-    //   this.arr2.push({
-    //     'criteria':"0",
-    //   });
-    //   console.log(this.arr2);
-    // }
+ 
 
   Edit(val){
-     this.EditRowId = val;
-     //console.log("function called");
-     
+     this.EditRowId = val;   
   }
 
 
   save(formData) {
     console.log(formData.value);
-    this.DataSaved = 0;
-
     for(let i = 0  ; i < this.participant_data.length ; i++){
-      this.Participants.at(i).get('dataSaveCheck').value = true;
+      this.Participants.at(i).get('dataSaveCheck').setValue(true);
     }
+    console.log(this.myForm.value);
+    this._registrationService.register(this.myForm.value)
+      .subscribe(
+        response => console.log('Success!' , response),
+        error => console.error('Error!', error)
+      );
   }
   saveCandidateForm(CandidateForm , i){
     console.log(CandidateForm.value);
-    this.Participants.at(i).get('dataSaveCheck').value = true;
+    this.Participants.at(i).get('dataSaveCheck').setValue(true);
+    this._registrationService.register(CandidateForm.value)
+    .subscribe(
+      response => console.log('Success!' , response),
+      error => console.error('Error!', error)
+    );
   }
   changeButtonColor(i){
-    this.Participants.at(i).get('dataSaveCheck').value = false;
-    //console.log(this.Participants.at(i).get('dataSaveCheck').value);
+    this.Participants.at(i).get('dataSaveCheck').setValue(false);
   }
 
 }
